552da80670784f7149afe4f5d19a9ebf54b3e83a
PietroUngar <pietro.ungar@unifi.it>
2021-03-22 16:30:48 +0100

    lincense information added

--- setup.py
+++ setup.py
@@ -3,21 +3,27 @@ from setuptools import setup
 setup(
 
     name='3ETool',
-    version='0.0.1',
-    packages=['src', 'src.Tools', 'src.Tools.Other', 'src.Tools.GUIElements', 'src.Tools.CostCorrelations',
-              'src.Tools.CostCorrelations.CorrelationClasses', 'src.Tools.EESCodeGenerator', 'src.MainModules',
-              'src.BlockSubClasses', 'test'],
+    version='0.0.2',
+
+    packages=[
+
+        'src', 'src.Tools', 'src.Tools.Other', 'src.Tools.GUIElements', 'src.Tools.CostCorrelations',
+        'src.Tools.CostCorrelations.CorrelationClasses', 'src.Tools.EESCodeGenerator', 'src.MainModules',
+        'src.BlockSubClasses', 'test'
+    ],
+
     url='https://www.dief.unifi.it/vp-473-exergo-economic-analysis-software.html',
-    license='',
+    license='GNU GPLv3',
     author='Pietro Ungar',
     author_email='pietro.ungar@unifi.it',
     description='Tools for performing exergo-economic and exergo-environmental analysis',
-    install_requires = [
+
+    install_requires=[
 
         'cryptography>=3.4.6',
         'numpy>=1.20.1',
-        'PyQt5>=5.15.4',
         'pandas>=1.2.3',
+        'PyQt5>=5.15.4',
         'setuptools'
 
     ]
