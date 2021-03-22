292a408fde7a63567f7b9f4c2bf04ad51c153a4a
PietroUngar <pietro.ungar@unifi.it>
2021-03-22 17:44:08 +0100

    Modified Setup.py (is IMPOSSIBLE to import '3ETool' package, renamed in 'EEETool')

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
 
     name='EEETool',
-    version='0.0.2',
+    version='0.0.3',
     license='GNU GPLv3',
 
     author='Pietro Ungar',
