485e550e7bf3795849d29d24e400751a7cc5dfd2
PietroUngar <pietro.ungar@unifi.it>
2022-01-11 15:32:32 +0100

    V0.3.3 bug fixed

--- setup.py
+++ setup.py
@@ -1,31 +1,63 @@
 from EEETools.version import VERSION
 from setuptools import setup
-from os import path
-import platform
+from os import path, listdir
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-install_requires = [
 
-    'googledrivedownloader',
-    'future~=0.18.2',
-    'PyQt5~=5.15.4',
-    'pandas~=1.2.3',
-    'openpyxl~=3.0.7',
-    'cryptography~=3.4.6',
-    'pyvis~=0.1.9',
-    'rply~=0.7.8',
-    'setuptools~=54.2.0',
-    'xlrd~=1.2.0',
-    'numpy'
+def __get_install_requires():
 
-]
+    install_requires = [
 
-if platform.system() == "Windows":
+        'googledrivedownloader',
+        'future~=0.18.2',
+        'PyQt5~=5.15.4',
+        'pandas~=1.2.3',
+        'openpyxl~=3.0.7',
+        'cryptography~=3.4.6',
+        'pyvis~=0.1.9',
+        'rply~=0.7.8',
+        'setuptools~=54.2.0',
+        'xlrd~=1.2.0',
+        'numpy'
 
-    install_requires.append('pywin32')
+    ]
+
+    import platform
+
+    if platform.system() == "Windows":
+        install_requires.append('pywin32')
+
+    return install_requires
+
+
+def __get_packages() -> list:
+
+    ROOT_DIR = path.join(path.dirname(__file__), "EEETools")
+    packages = append_sub_dir(ROOT_DIR, list())
+    return packages
+
+
+def append_sub_dir(element_path, input_list: list, parent_name=None) -> list:
+
+    if path.isdir(element_path):
+
+        name = path.basename(element_path)
+
+        if "__" not in name:
+
+            if parent_name is not None:
+                name = "{}.{}".format(parent_name, name)
+
+            input_list.append(name)
+
+            for sub_name in listdir(element_path):
+                input_list = append_sub_dir(path.join(element_path, sub_name), input_list, parent_name=name)
+
+    return input_list
+
 
 setup(
 
@@ -51,15 +83,9 @@ setup(
 
     },
 
-    packages=[
+    packages=__get_packages(),
 
-        'EEETools', 'EEETools.Tools', 'EEETools.Tools.Other', 'EEETools.Tools.GUIElements', 'EEETools.Tools.CostCorrelations',
-        'EEETools.Tools.CostCorrelations.CorrelationClasses', 'EEETools.Tools.EESCodeGenerator', 'EEETools.MainModules',
-        'EEETools.BlockSubClasses', 'test'
-
-    ],
-
-    install_requires=install_requires,
+    install_requires=__get_install_requires(),
 
     classifiers=[
 
@@ -68,6 +94,6 @@ setup(
         'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
-      ]
+    ]
 
 )
